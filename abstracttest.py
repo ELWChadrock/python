8126e92be7e9560462a822b8aa1a373d6af148d6
ELWChadrock <68598620+ELWChadrock@users.noreply.github.com>
2020-10-06 20:43:19 -0600

    Add files via upload

--- abstracttest.py
+++ abstracttest.py
@@ -1,22 +1,24 @@
-from abc import ABC, abstractmethod 
-  
-class Polygon(ABC):
-    
-    def numofsides(self): 
-        pass
-  
-class Triangle(Polygon): 
+from abc import ABC, abstractmethod
+class Bro(ABC):
+  def bro_slip(self, amount):
+    print('Purchase of amount- ', amount)
+  @abstractmethod
+  def bro(self, amount):
+    pass
 
-    def numofsides(self): 
-        print("I have 3 sides") 
-  
-class Pentagon(Polygon): 
-  
-    def numofsides(self): 
-        print("I have 5 sides") 
- 
-V = Triangle() 
-V.numofsides() 
-  
-K = Pentagon() 
-K.numofsides() 
+class BroNumber(Bro):
+  def bro(self, amount):
+    print('Bro status of- ', amount)
+
+class BroLegitNum(Bro):
+  def bro(self, amount):
+    print('Bro legit like- ', amount)
+
+obj = BroNumber()
+obj.bro(100)
+obj.bro_slip(100)
+print(isinstance(obj, Bro))
+obj = BroLegitNum()
+obj.bro(200)
+obj.bro_slip(200)
+print(isinstance(obj, Bro))
