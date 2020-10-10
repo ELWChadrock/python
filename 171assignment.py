ec3df97d439c4226fc549d7051aeb5cab6e784d5
ELWChadrock <68598620+ELWChadrock@users.noreply.github.com>
2020-10-10 11:16:46 -0600

    Add files via upload

--- 171assignment.py
+++ 171assignment.py
@@ -1,19 +1,23 @@
 import os
-open (fileList = ('information.docx','Hello.txt','myImage.png', \
-            'myMovie.mpg','World.txt','data.pdf','myPhoto.jpg'))
+fileList = ['information.docx','Hello.txt','myImage.png', \
+            'myMovie.mpg','World.txt','data.pdf','myPhoto.jpg']
+for v in fileList:
+    print(v)
+    if v == ".txt":
 
 import sqlite3
 
-conn = sqlite3.conenct('fake.db')
+
+conn = sqlite3.connect('fake.db')
 
 with conn:
     cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS tbl_coolstuff ( \
+    cur.execute("CREATE TABLE IF NOT EXISTS tbl_filetest ( \
         ID INTEGER PRIMARY KEY AUTOINCREMENT, \
-        col_dude TEXT, \
-        col_bro TEXT, \
-        col_guy TEXT \
+        col_file TEXT, \
+        col_ID INT \
         )")
+    
     conn.commit()
 conn.close()
 
@@ -21,22 +25,35 @@ conn = sqlite3.connect('fake.db')
 
 with conn:
     cur = conn.cursor()
-    cur.execute("INSERT INTO tbl_coolstuff(col_dude, col_bro, col_guy) VALUES (?,?,?)", \
-                ('I', 'Dont', 'know'))
-    cur.execute("INSERT INTO tbl_coolstuff(col_dude, col_bro, col_guy) VALUES (?,?,?)", \
-                ('You', 'Pretty', 'Neat'))
-    cur.execute("INSERT INTO tbl_coolstuff(col_dude, col_bro, col_guy) VALUES (?,?,?)", \
-                ('Oh', 'Hi', 'Mark'))
+    cur.execute("INSERT INTO tbl_filetest(col_file, col_ID) VALUES (?,?)", \
+                ('Hello.txt', 1))
+    cur.execute("INSERT INTO tbl_filetest(col_file, col_ID) VALUES (?,?)", \
+                ('World.txt', 2))
     conn.commit()
 conn.close()
 
 
-conn = sqlite3.connect('fake.db')
 
-with conn:
-    cur = conn.cursor()
-    cur.execute("SELECT col_dude,col_bro,col_guy, FROM tbl_coolstuff WHERE col_dude = 'Oh'")
-    varPerson = our.fetchall()
-    for item in varPerson:
-        msg = "First Name: {}\nLast Name: {}\nOther: {}".format(item[0],item[1],item[2])
-    print(msg)
+
+    
+##    cur.execute("INSERT INTO tbl_coolstuff(col_dude, col_bro, col_guy) VALUES (?,?,?)", \
+##                ('I', 'Dont', 'know'))
+##    cur.execute("INSERT INTO tbl_coolstuff(col_dude, col_bro, col_guy) VALUES (?,?,?)", \
+##                ('You', 'Pretty', 'Neat'))
+##    cur.execute("INSERT INTO tbl_coolstuff(col_dude, col_bro, col_guy) VALUES (?,?,?)", \
+##                ('Oh', 'Hi', 'Mark'))
+##    conn.commit()
+##conn.close()
+##
+##
+##conn = sqlite3.connect('fake.db')
+##
+##with conn:
+##    cur = conn.cursor()
+##    cur.execute("SELECT col_dude,col_bro,col_guy, FROM tbl_coolstuff WHERE col_dude = 'Oh'")
+##    varPerson = our.fetchall()
+##    for item in varPerson:
+##        msg = "First Name: {}\nLast Name: {}\nOther: {}".format(item[0],item[1],item[2])
+##    print(msg)
+##
+##
