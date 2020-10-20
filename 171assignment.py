187cdbbbb5a9a34c2c0a2f059f1ce0dbe9f0e6d9
ELWChadrock <68598620+ELWChadrock@users.noreply.github.com>
2020-10-19 22:19:51 -0600

    Add files via upload

--- 171assignment.py
+++ 171assignment.py
@@ -1,14 +1,15 @@
 import os
-fileList = ['information.docx','Hello.txt','myImage.png', \
-            'myMovie.mpg','World.txt','data.pdf','myPhoto.jpg']
-for v in fileList:
-    print(v)
-    if v == ".txt":
-
 import sqlite3
 
+fileList = ['information.docx','Hello.txt','myImage.png', \
+            'myMovie.mpg','World.txt','data.pdf','myPhoto.jpg']
 
 conn = sqlite3.connect('fake.db')
+
+
+for v in fileList:
+    if v.endswith(".txt"):
+        print(v)
 
 with conn:
     cur = conn.cursor()
@@ -17,18 +18,9 @@ with conn:
         col_file TEXT, \
         col_ID INT \
         )")
+    cur.execute("INSERT INTO tbl_filetest(col_file, col_ID) VALUES (?,?)", \
+                (v,))
     
-    conn.commit()
-conn.close()
-
-conn = sqlite3.connect('fake.db')
-
-with conn:
-    cur = conn.cursor()
-    cur.execute("INSERT INTO tbl_filetest(col_file, col_ID) VALUES (?,?)", \
-                ('Hello.txt', 1))
-    cur.execute("INSERT INTO tbl_filetest(col_file, col_ID) VALUES (?,?)", \
-                ('World.txt', 2))
     conn.commit()
 conn.close()
 
