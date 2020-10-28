730078a5f5d61ca8fe19b282c149774ab466f23a
ELWChadrock <68598620+ELWChadrock@users.noreply.github.com>
2020-10-27 21:38:47 -0600

    Add files via upload

--- 171assignment.py
+++ 171assignment.py
@@ -1,25 +1,37 @@
 import os
 import sqlite3
 
-fileList = ['information.docx','Hello.txt','myImage.png', \
-            'myMovie.mpg','World.txt','data.pdf','myPhoto.jpg']
-
 conn = sqlite3.connect('fake.db')
 
+with conn:
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS tbl_newfiletest ( \
+        ID INTEGER PRIMARY KEY AUTOINCREMENT, \
+        col_file TEXT \
+        )")
+    conn.commit()
+conn.close()
+
+
+fileList = ['information.docx','Hello.txt','myImage.png', \
+            'myMovie.mpg','World.txt','data.pdf','myPhoto.jpg']
 
 for v in fileList:
     if v.endswith(".txt"):
         print(v)
 
+conn = sqlite3.connect('fake.db')
+
 with conn:
     cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS tbl_filetest ( \
+    cur.execute("CREATE TABLE IF NOT EXISTS tbl_newfiletest ( \
         ID INTEGER PRIMARY KEY AUTOINCREMENT, \
-        col_file TEXT, \
-        col_ID INT \
+        col_file TEXT \
         )")
-    cur.execute("INSERT INTO tbl_filetest(col_file, col_ID) VALUES (?,?)", \
-                (v,))
+    cur.execute("INSERT INTO tbl_newfiletest(col_file) VALUES (?)", \
+                ('Hello.txt'))
+    cur.execute("INSERT INTO tbl_newfiletest(col_file) VALUES (?)", \
+                ('World.txt'))
     
     conn.commit()
 conn.close()
